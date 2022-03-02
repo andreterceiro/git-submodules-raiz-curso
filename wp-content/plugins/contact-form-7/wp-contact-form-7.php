b81562c3f0e450e419e38be53b7f35564a4974ed
André de Paula Terceiro <andre@terceiro.com.br>
2022-03-02 09:49:04 -0300

    feat: submodule add

--- wp-content/plugins/contact-form-7/wp-contact-form-7.php
+++ wp-content/plugins/contact-form-7/wp-contact-form-7.php
@@ -1,8 +1,8 @@
 <?php
 /*
-Plugin Name: Contact Form 7
+Plugin Name: Contact Form 8
 Plugin URI: https://contactform7.com/
-Description: Just another contact form pronto plugin. Simple but flexible.
+Description: Just another contact form plugin. Simple but flexible.
 Author: Takayuki Miyoshi
 Author URI: https://ideasilo.wordpress.com/
 Text Domain: contact-form-7
